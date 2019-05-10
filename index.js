f228c479d428d7a577ed46c39f39c09c48589ce4
Rafael Cerri <rafaelbcerri@hotmail.com>
2019-05-10 14:37:45 -0300

    Feedback

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 function extractLinksFromMd(value) {
-  return ((typeof value === 'string') && (value !== '') && (value !== isNaN) && (value.length > 1)) ? extract(value) : false;
+  const isTextValid = (typeof value === 'string') && (value !== '') && (value !== isNaN) && (value.length > 1);
+  return isTextValid ? extract(value) : false;
 }
 
 function extract(value) {
   let links = [];
-  class Link {
-    constructor(text, href) {
-      this.href = href;
-      this.text = text;
-    }
-  }  
+
   const regex =  /(\[(.*?)\])|(https?|ftp):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?/gi;
   if (value.match(regex).length < 1) return links;
   value.match(regex).reverse().map((x, idx) => (idx % 2 === 0) ? links.push(new Link(value.match(regex).reverse()[idx + 1], x)) : null);   
   return (JSON.stringify(links, null, 1));
 }
+
+class Link {
+  constructor(text, href) {
+    this.href = href;
+    this.text = text;
+  }
+} 
 
 module.exports = extractLinksFromMd;
